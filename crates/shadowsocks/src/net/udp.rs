30408d4f49eb2cdee4308b7de065a875d2b57175
Enoch Carter <Enoch.Carter@snapone.com>
2025-02-24 18:57:06 -0700

    added icegate/rust-tun submodule

--- crates/shadowsocks/src/net/udp.rs
+++ crates/shadowsocks/src/net/udp.rs
@@ -5,7 +5,8 @@
     target_os = "android",
     target_os = "macos",
     target_os = "ios",
-    target_os = "freebsd"
+    target_os = "freebsd",
+    target_os = "tvos",
 ))]
 use std::io::{ErrorKind, IoSlice, IoSliceMut};
 use std::{
@@ -30,7 +31,8 @@ use futures::ready;
     target_os = "android",
     target_os = "macos",
     target_os = "ios",
-    target_os = "freebsd"
+    target_os = "freebsd",
+    target_os = "tvos",
 ))]
 use tokio::io::Interest;
 use tokio::{io::ReadBuf, net::ToSocketAddrs};
@@ -48,7 +50,8 @@ use super::{
     target_os = "android",
     target_os = "macos",
     target_os = "ios",
-    target_os = "freebsd"
+    target_os = "freebsd",
+    target_os = "tvos",
 ))]
 pub struct BatchSendMessage<'a> {
     /// Optional target address
@@ -65,7 +68,8 @@ pub struct BatchSendMessage<'a> {
     target_os = "android",
     target_os = "macos",
     target_os = "ios",
-    target_os = "freebsd"
+    target_os = "freebsd",
+    target_os = "tvos"
 ))]
 pub struct BatchRecvMessage<'a> {
     /// Peer address
@@ -348,7 +352,8 @@ impl UdpSocket {
         target_os = "android",
         target_os = "ios",
         target_os = "macos",
-        target_os = "freebsd"
+        target_os = "freebsd",
+        target_os = "tvos"
     ))]
     pub fn poll_batch_recv(
         &self,
